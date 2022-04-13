8d0e675e5106a42107b0e5954ef436c3aea2d302
sy <1182747827@qq.com>
2022-04-13 15:53:47 +0800

    GAN代码优化

--- src/main/GAN/test.py
+++ src/main/GAN/test.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
 from models.generator import Generator
-from gan_config import *
-from preprocessing import load_tokenizer
+from gan_config import BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, pretrained_generator_file
+import numpy as np
+
+import os
+import sys
+# 项目路径,将项目路径保存
+project_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.append(project_path)
+
+result_dir = project_path + os.sep + 'result' + os.sep
 
 physical_devices = tf.config.experimental.list_physical_devices("GPU")
 if len(physical_devices) > 0:
@@ -10,17 +18,28 @@ if len(physical_devices) > 0:
 
 
 def load_generator():
-    generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH)
+    generator = Generator(BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH)
     generator.load(pretrained_generator_file)
     return generator
 
-def decode_to_sentences(sentences):
-    return tokenizer.sequences_to_texts(sentences)
-
+def sequences_to_texts(sentences):
+    word_dict = np.load(result_dir + 'w2dic.npy', allow_pickle=True).item()
+    key_list = list(word_dict.keys())
+    vocabulary = word_dict.values()
+    value_list = list(vocabulary)
+    results = []
+    for each_sentence in sentences:
+        x = ''
+        for each_word in each_sentence:
+            if each_word in vocabulary:
+                x += key_list[value_list.index(each_word)]
+            else:
+                x += ' '
+        results.append(x)
+    return results
 
 if __name__ == "__main__":
     generator = load_generator()
-    tokenizer = load_tokenizer()
     generated_sentences = generator.generate_one_batch().numpy()
-    sentences = tokenizer.sequences_to_texts(generated_sentences)
+    sentences = sequences_to_texts(generated_sentences)
     print(*sentences, sep='\n')
